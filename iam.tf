5186e9cb9e92d712959b53fbe0e3fb190305d25e
pravin1710 <113180627+pravin1710@users.noreply.github.com>
2024-07-01 16:03:48 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,41 +1,24 @@
-resource "aws_ecs_service" "calendar_ecs-service" { //configuration required for calendar ecs service 
-  name                               = "${var.service-2}-ecs_service"
-  cluster                            = aws_ecs_cluster.aws-ecs-cluster.id
-  task_definition                    = aws_ecs_task_definition.calendar-service_ecs_task.arn
-  deployment_maximum_percent         = 200
-  deployment_minimum_healthy_percent = 100
-  launch_type                        = "FARGATE"
-  scheduling_strategy                = "REPLICA"
-  desired_count                      = 2
-  enable_ecs_managed_tags            = true
-  force_new_deployment               = true
-
-  deployment_circuit_breaker {
-    enable   = false
-    rollback = false
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "${var.app_name}-execution-task-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  tags = {
+    Name        = "${var.app_name}-iam-role"
+    Environment = var.app_environment
   }
+}
 
-  deployment_controller {
-    type = "ECS"
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
   }
+}
 
-  network_configuration {
-    subnets          = aws_subnet.private.*.id
-    assign_public_ip = false
-    security_groups = [
-      aws_security_group.service_security_group.id,
-      aws_security_group.load_balancer_security_group.id
-    ]
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.calander_service_fargates.arn
-    container_name   = "${var.service-2}-container"
-    container_port   = 8080
-  }
-
-  lifecycle {
-    ignore_changes = [desired_count, task_definition]
-  }
-
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
