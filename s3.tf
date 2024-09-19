bfc8def11903e3682ca08e4b545ca3572e63c92e
pravin1710 <113180627+pravin1710@users.noreply.github.com>
2024-09-19 15:47:35 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,22 +1,11 @@
 resource "aws_s3_bucket" "services_env_bucket" {
-  bucket = "services-env-variabsles" # Specify your bucket name
+  bucket = "services-envs-variabsles" # Specify your bucket name
 }
 
 resource "aws_s3_bucket_versioning" "s3_env_variables" {
   bucket = aws_s3_bucket.services_env_bucket.id
   versioning_configuration {
     status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
-  bucket = aws_s3_bucket.services_env_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.staging_leadsigma_kms_key.arn
-      sse_algorithm     = "aws:kms"
-    }
   }
 }
 
