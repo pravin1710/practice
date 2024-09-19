56a639480bd029321783273454a5ce521620d444
pravin1710 <113180627+pravin1710@users.noreply.github.com>
2024-09-19 15:03:32 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -14,7 +14,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
+      kms_master_key_id = aws_kms_key.staging_leadsigma_kms_key.arn
+      sse_algorithm     = "aws:kms"
     }
   }
 }
